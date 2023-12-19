ed841e2a21bab9b3d11604e74fc847aa3f935a32
anjanasri13 <srianjana55@gmail.com>
2023-12-19 13:33:22 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import LottieView from 'lottie-react-native';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View style={{marginTop:50,alignItems: "center", justifyContent: "center"}}>
+      <LottieView source={require("./assets/merry1.json")} autoPlay loop style={{height: 150, width: 150}} />
+      <LottieView source={require("./assets/gifts.json")} autoPlay loop style={{height: 150, width: 150}} />
+      {/* <LottieView source={require("./assets/lottie.json")} autoPlay loop style={{height: 150, width: 150}} /> */}
+    </View>
+  );
+}
+
+
+export default App;
